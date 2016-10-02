57d7e134c03336d1ae9e1cc580b4ec5dc3434e6a
Zoker <kaixuanguiqu@gmail.com>
2016-10-02 20:56:21 +0800

    add user create with encrypt password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,22 @@
 class User < ApplicationRecord
 
-  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+  attr_accessor :password
+
+  before_save :encrypt_password
+  after_save :empty_password
+
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: EMAIL_REGEX
-  validates :password, presence: true, length: 6..20, confirmation: true
+  validates :password, presence: true, length: 6..20
+
+  def encrypt_password
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password_digest = BCrypt::Engine.hash_secret(password, password_salt)
+  end
+
+  def empty_password
+    self.password = nil
+  end
 
 end
