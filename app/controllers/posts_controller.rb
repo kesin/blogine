3aa092a78ab9fd7ee7cdae999c83c71db7dcdad2
Zoker <kaixuanguiqu@gmail.com>
2016-10-02 16:57:53 +0800

    complete md editor post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :ident, :content, :column_id, :status)
+      params.require(:post).permit(:title, :ident, :content, :html_content, :column_id, :status)
     end
 end
