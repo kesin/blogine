3f5a31a073d0aca456eb3629f5fb8a4b519b7501
Zoker <kaixuanguiqu@gmail.com>
2016-10-03 01:52:18 +0800

    fixed login bug

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,7 +3,7 @@ module SessionsHelper
   def current_user
     return @current_user if defined? @current_user
     if session[:user_id]
-      @current_user ||= User.find_by_id(id: session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
     else
       nil
     end
