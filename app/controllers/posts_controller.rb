8a9ba6f6f5a108703f144174b03e1ea698a58d15
Zoker <kaixuanguiqu@gmail.com>
2016-10-03 10:42:34 +0800

    remove comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,34 +2,26 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user, except: [:index, :show]
 
-  # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts
-  # POST /posts.json
   def create
     @post = Post.new(post_params)
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to @post, notice: '创建成功！' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
@@ -38,12 +30,10 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.html { redirect_to @post, notice: '更新成功！' }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
@@ -52,12 +42,10 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to root_path, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: '删除成功！' }
       format.json { head :no_content }
     end
   end
