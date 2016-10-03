a7f6c15561ee9b5ecc53a804bbef297ce5196f1c
Zoker <kaixuanguiqu@gmail.com>
2016-10-03 10:35:41 +0800

    add authenticate method to check permission

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  include SessionsHelper
+
+  def authenticate_user
+    return redirect_to login_path, alert: '请登录后进行操作。' unless current_user
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,5 @@
 class SessionsController < ApplicationController
 
-  include SessionsHelper
-
   def new
     # new session page
   end
