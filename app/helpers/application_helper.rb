44402b32156084506c760d1617089672b61b10f5
Zoker <kaixuanguiqu@gmail.com>
2016-10-22 00:43:32 +0800

    enhance dashboard style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def title(title)
+    content_for(:title) { title }
+  end
+
+  def meta_keywords(keywords)
+    content_for(:meta_keywords) { keywords }
+  end
+
+  def meta_description(description)
+    content_for(:meta_description) { description }
+  end
 end
