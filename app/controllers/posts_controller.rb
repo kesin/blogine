790c2e89cf699940fd3525257dc289d9a542dbea
Zoker <kaixuanguiqu@gmail.com>
2016-10-22 23:21:23 +0800

    move post c to admin

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,63 +1,10 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user, except: [:index, :show]
 
   def index
     @posts = Post.all
   end
 
   def show
+    @post = Post.find_by_ident(params[:id])
   end
-
-  def new
-    @post = Post.new
-  end
-
-  def edit
-  end
-
-  def create
-    @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: '创建成功！' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: '更新成功！' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to root_path, notice: '删除成功！' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find_by_ident(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:title, :ident, :intro, :content, :html_content, :column_id, :status)
-    end
 end
