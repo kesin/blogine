146d4fc37eccdbf51a970cda37054de0ccd73a18
Zoker <kaixuanguiqu@gmail.com>
2016-10-23 15:03:41 +0800

    add post tag assosition and tagsinput js

--- app/models/post_tag_ref.rb
+++ app/models/post_tag_ref.rb
@@ -1,2 +1,4 @@
 class PostTagRef < ApplicationRecord
+  belongs_to :tag
+  belongs_to :post
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,8 @@
 class Tag < ApplicationRecord
 
+  has_many :post_tag_refs
+  has_many :posts, through: :post_tag_refs
+
   def to_param
     name
   end
