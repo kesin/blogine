c0ac882b46b40894a66d897b43460d894f5bf2bf
Zoker <kaixuanguiqu@gmail.com>
2017-08-12 00:05:46 +0800

    add author avatar for admin

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,5 +1,14 @@
 module CommentsHelper
+
   def author_class(status)
     status == 2 ? 'by-author' : ''
+  end
+
+  def comment_avatar(status, email)
+    if status == 2
+      Settings.blogine.avatar
+    else
+      gravatar_image(email)
+    end
   end
 end
